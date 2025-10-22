9be4525260b938f038df11fe68d98e5695ca46f2
Feirry <zeaitersarah7@gmail.com>
2025-10-22 16:20:43 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+app.use(cors());
+app.use(express.static(path.join(__dirname, '/'))); // serve index.html
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Frontend running at http://localhost:${PORT}`));
